724ae566c1b583ec4d0127bd6e822be06201af7b
MBENGONO AKOA <Salomonakoa44@gmail.com>
2024-10-17 22:59:19 +0200

    creation of UserModel and SkillModel

---
+++ Domain/Model/SkillModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Model
+{
+    public class SkillModel
+    {
+        public int SkillId { get; set; } = default(int);
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Domain/Model/UserModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Domain.Model
+{
+    public class UserModel
+    {
+        public int UserId { get; set; } = default;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Profession { get; set; } = string.Empty;
+
+        public int YearsOfExperience { get; set; }
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Sex { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+        public byte[] Resume { get; set; }
+
+        public List<SkillModel> Skills { get; set; } = new List<SkillModel>();
+
+        public enum PreferredJobType
+        {
+            Fulltime,
+            PartTime,
+            Contract,
+            Remote
+        }
+
+        public string LinkedInProfile { get; set; } = string.Empty;
+    }
+}
