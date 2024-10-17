9dd97831d264f8f493bf54bbd6852502806ddfa9
MBENGONO AKOA <Salomonakoa44@gmail.com>
2024-10-17 23:10:37 +0200

    configs

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f464763dee5e920cc6912b5b1edeeec485a63999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724ae566c1b583ec4d0127bd6e822be06201af7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
