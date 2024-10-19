d576eda964a0f7d08ad3c08cee51f90eddbfc717
MBENGONO AKOA <Salomonakoa44@gmail.com>
2024-10-19 15:28:43 +0200

    creation of user service

---
+++ Application/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿namespace Application.Services;
+
+using Domain.Models;
+using Domain.Services;
+using System.Collections.Generic;
+
+public class UserService :IUserService
+{
+    public UserModel AddUser(UserModel userToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteUser(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UserModel GetUser(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<UserModel> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int UpdateUser(UserModel userModelToUpdate)
+    {
+        throw new NotImplementedException();
+    }
+}
