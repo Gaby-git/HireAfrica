a6d1fbb22412791a3587266eebd8981c94cf97a2
MBENGONO AKOA <Salomonakoa44@gmail.com>
2024-10-19 15:29:05 +0200

    update of domain layer

--- Domain/Model/SkillModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Model
-{
-    public class SkillModel
-    {
-        public int SkillId { get; set; } = default(int);
-        public string Name { get; set; } = string.Empty;
-    }
-}
---
+++ Domain/Models/SkillModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models;
+
+public class SkillModel
+{
+    public int SkillId { get; set; } = default(int);
+    public string Name { get; set; } = string.Empty;
+}
