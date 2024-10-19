162e1d65ba4adbecdc8b10b76beebb9953129984
MBENGONO AKOA <Salomonakoa44@gmail.com>
2024-10-19 15:26:21 +0200

    Configuration of infrastructure repository

---
+++ Infrastructure.SqlServer/Stores/UserStore.cs
@@ -0,0 +1,33 @@
+﻿namespace Infrastructure.SqlServer.Stores;
+
+using Domain.Models;
+using Domain.Stores;
+using System.Collections.Generic;
+
+public class UserStore :IUserStore
+{
+    public UserModel AddUser(UserModel userToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteUser(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UserModel GetUser(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<UserModel> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int UpdateUser(UserModel userModelToUpdate)
+    {
+        throw new NotImplementedException();
+    }
+}
