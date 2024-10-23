726dfdaa9091981544698a2bf35a945f7c5cc3e3
LekaneGabriel <lekanegabriel199@gmail.com>
2024-10-23 07:31:40 +0200

    Class for the Job Listing and proparties

---
+++ Domain/Models/JobListingModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Models;
+
+public class JobListingModel
+{
+    public int JobId { get; set; } = default;
+    public string JobTitle { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public decimal Salary { get; set; } = default;
+    public EmploymentType Employment { get; set; }
+    public int HoursOfWork { get; set; }
+    public List<string> RequiredSkills { get; set; } = default!;
+    public string EducationLevel { get; set; } = string.Empty;
+    public ExperienceLevel Experience { get; set; }
+}
+
+public enum EmploymentType
+{
+    FullTime,
+    PartTime,
+    Contract
+}
+
+public enum ExperienceLevel
+{
+    Entry,
+    Mid,
+    Senior
+}
