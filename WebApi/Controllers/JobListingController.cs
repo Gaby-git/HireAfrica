2ce22b2a8a19778dd0b7a303d68eed79f2a66d99
LekaneGabriel <lekanegabriel199@gmail.com>
2024-10-30 20:04:29 +0100

    Implementing classic CRUD functionalities for JobListing without any verification yet.

---
+++ WebApi/Controllers/JobListingController.cs
@@ -0,0 +1,64 @@
+﻿namespace WebApi.Controllers;
+
+using Domain.Models;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JobListingController :ControllerBase
+{
+    private readonly IJobListingService _jobListingService;
+    public JobListingController(IJobListingService jobListingService)
+        => _jobListingService = jobListingService;
+
+    [HttpPost]
+    public IActionResult AddJob(JobListingModel jobListingModel)
+    {
+        var addJob = 
+                 _jobListingService.AddJob(jobListingModel);
+
+            return CreatedAtAction(
+                  nameof(GetJobById), new { jobId = addJob.JobId }, addJob);
+    }
+
+    [HttpGet]
+    public IActionResult GetJobList()
+    {
+        var jobList = 
+                   _jobListingService.GetJobList();
+            return Ok(
+                  jobList);
+
+    }
+
+    [HttpGet("{jobId:int}")]
+    public IActionResult GetJobById(int jobId)
+    {
+        var job = 
+                _jobListingService.GetJob(jobId);
+
+            return Ok(
+                  job);
+    }
+
+    [HttpPut("{jobId:int}")]
+    public IActionResult UpdateJob(int jobId, JobListingModel jobListingModel)
+    {
+
+        var updatedJob = 
+                  _jobListingService.UpdateJob(jobId, jobListingModel);
+
+            return Ok(
+                  updatedJob);
+
+    }
+
+    [HttpDelete("{jobId:int}")]
+    public IActionResult DeleteJob(int jobId)
+    {
+        _jobListingService.DeleteJob(jobId);
+
+        return NoContent();
+    }
+}
