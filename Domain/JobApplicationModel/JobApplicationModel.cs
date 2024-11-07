b3531dfadbe72ed06c3475feeb45e70eb3184e3d
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-07 15:31:58 +0100

    Creating a separate model for the class JobApplicationModel and PortfolioItem is a best practice for clean code. This part serves the applicant by providing information about their resume and portfolio.

---
+++ Domain/JobApplicationModel/JobApplicationModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.JobApplicationModels;
+public class JobApplicationModel
+{
+    public int ApplicationId { get; set; } = default;
+    public DateTime ApplicationDate { get; set; }
+    public string CandidateName { get; set; } = string.Empty;
+    public byte[] CVFile { get; set; } = default!;
+    public List<PortfolioItem> Portfolio { get; set; } = new List<PortfolioItem>();
+}
+
+public class PortfolioItem
+{
+    public string Link { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
