ac1c5bcab719f410ebe955478283121c5b269523
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-13 09:56:02 +0100

    creation of JobApplicationModel and Portfolio to manage users application.

---
+++ Domain/Models/JobApplicationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models;
+using System;
+using System.Collections.Generic;
+
+public class JobApplicationModel
+{
+    public int ApplicationId { get; set; }
+    public int UserId { get; set; }
+    public int JobId { get; set; }
+    public DateTime ApplicationDate { get; set; }
+    public byte[] CVFile { get; set; } = default!;
+    public List<PortfolioItemModel> Portfolio { get; set; } = new List<PortfolioItemModel>();
+}
---
+++ Domain/Models/PortfolioItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models;
+public class PortfolioItemModel
+{
+    public int ItemId { get; set; }
+    public int UserId { get; set; }
+    public string Link { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+
+}
